06e73ba47156c9f59f5843b0d2fc9e21acb831a4
SiEr0212 <simonertel@gmx.de>
2020-07-30 10:28:17 +0200

    updating to master1

---
+++ client/build/precache-manifest.93e4755fd10cc4abea3e87cbc295411a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850531d252d60bc5e30d311402caa0f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9408f8735a4bcf1f2c3",
+    "url": "/static/css/2.40068ff7.chunk.css"
+  },
+  {
+    "revision": "95e3eb82c87c30cc17a8",
+    "url": "/static/css/main.eb95c684.chunk.css"
+  },
+  {
+    "revision": "f9408f8735a4bcf1f2c3",
+    "url": "/static/js/2.b89d7900.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b89d7900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e3eb82c87c30cc17a8",
+    "url": "/static/js/main.70678ee0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
