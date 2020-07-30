83745b46251ba84fb04bf4f202534b50d949979a
luisschmitz <luis.schmitz@web.de>
2020-07-30 17:13:30 +0200

    Edit field added

---
+++ client/build/precache-manifest.f5fd80d53f905c1136bb9cfd5f4c758b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78a5f5f7119e458539265d4546d8724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "212b200b3c067d0394c7",
+    "url": "/static/css/2.40b7ee1d.chunk.css"
+  },
+  {
+    "revision": "006ca92b09a968a4a2a9",
+    "url": "/static/css/main.62fd95c9.chunk.css"
+  },
+  {
+    "revision": "212b200b3c067d0394c7",
+    "url": "/static/js/2.e00cd344.chunk.js"
+  },
+  {
+    "revision": "5599e968862052a1e150ba50f6354fc6",
+    "url": "/static/js/2.e00cd344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006ca92b09a968a4a2a9",
+    "url": "/static/js/main.080238da.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8b4f2d2ab6c733a2db82700365eb6cba",
+    "url": "/static/media/IMG_9232.8b4f2d2a.JPG"
+  },
+  {
+    "revision": "ee2be3697a4761c3ab4b5fd7cab01fb3",
+    "url": "/static/media/LOGOMYTENT.ee2be369.png"
+  }
+]);
